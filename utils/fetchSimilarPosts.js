9660a37c734342faddce5b9fa386e723907072ab
Hamza Rafique <hamzarafique964@gmail.com>
2024-06-28 15:13:55 +0300

    posts module is deleted

--- utils/fetchSimilarPosts.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function fetchSimilarPosts(categories) {
-  const response = await axios.get(
-    `https://api.your-blog-data.com/posts?categories=${categories.join(",")}`
-  );
-  return response.data;
-}
